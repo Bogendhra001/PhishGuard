0a23cb98dbf508322529946ef68fcbffc396142d
Bogendhra001 <msaibogendhrareddy@gmail.com>
2024-02-25 16:35:44 +0530

    first

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'FrontEnd\phishguard\build'),]
+STATICFILES_DIRS = [os.path.join(
+    BASE_DIR, 'project1\FrontEnd\phishguard\build'),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
