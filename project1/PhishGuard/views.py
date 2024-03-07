3337aad83d182d2e72ae6881af1d7efe8533af57
Bogendhra001 <msaibogendhrareddy@gmail.com>
2024-03-07 17:05:00 +0530

    scrapcode removed

--- project1/PhishGuard/views.py
+++ project1/PhishGuard/views.py
@@ -1,18 +1,3 @@
-# from django.shortcuts import render
-# from django.template import loader
-# from django.http import HttpResponse
-# import extract from '/project1\extration.py'
-# # Create your views here.
-
-
-# def index(request):
-#     template = loader.get_template('index.html')
-#     return HttpResponse(template.render())
-
-
-# def classify(request,dynamic_stirg):
-#     y=extract(dynamic_stirg) #calling the function to get the result of classification
-#     return(y)
 
 
 from django.shortcuts import render
